72e9a78288f9c014c302448f38289c11119617ea
septimiy-severus <131580499+septimiy-severus@users.noreply.github.com>
2023-07-09 16:57:20 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 let burger = document.querySelector(".burger");
 let navigation = document.querySelector(".navigation");
+let body = document.querySelector("body")
 // console.log(burger);
 
 burger.addEventListener("click", function () {
     burger.classList.toggle("active")
     navigation.classList.toggle("active")
+    body.classList.toggle("hidden")
 })
